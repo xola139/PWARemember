815a39c6e8a79cec356b5cdeb146a8f03ac4cfc5
Raul Corona <xola139@gmail.com>
2018-10-24 12:42:24 -0500

    cambio name project heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/PWARemember'));
+app.use(express.static(__dirname + '/dist/pwaremember'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/PWARemember/index.html'));
+res.sendFile(path.join(__dirname+'/dist/pwaremember/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
